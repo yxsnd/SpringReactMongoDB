86eb4c40837fd7ad37456e6bdc0d67e1ba353721
yxsnd <yasindu.d@hotmail.com>
2024-01-12 14:05:17 -0500

    added post function to leave reviews on a given imdbid given in JSON

---
+++ movies/src/main/java/dev/ykd/movies/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.ykd.movies.repository;
+
+import dev.ykd.movies.model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ movies/src/main/java/dev/ykd/movies/service/ReviewService.java
@@ -0,0 +1,36 @@
+package dev.ykd.movies.service;
+
+import dev.ykd.movies.model.Movie;
+import dev.ykd.movies.model.Review;
+import dev.ykd.movies.repository.ReviewRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    private static Logger logger = LogManager.getLogger(ReviewService.class);
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+        try {
+            mongoTemplate.update(Movie.class)
+                    .matching(Criteria.where("imdbId").is(imdbId))
+                    .apply(new Update().push("reviewIds").value(review))
+                    .first();
+        } catch (Exception e) {
+            logger.error(e);
+        }
+        return review;
+    }
+}
